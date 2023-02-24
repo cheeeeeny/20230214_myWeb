e39ceaaf6e2a44d224c3e9bad7b2dd6f4d89397e
cheeeeeny <h_6420@naver.com>
2023-02-24 15:16:19 +0900

    수정

--- src/kh/common/jdbc/JdbcTemplate.java
+++ src/kh/common/jdbc/JdbcTemplate.java
@@ -7,12 +7,13 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class JdbcTemplate {
-
+//	lh.driver=oracle.jdbc.OracleDriver
+//			lh.url=jdbc:oracle:thin:@localhost:1521:xe
 	//DB연결
 	public static Connection getConnection() {
 		Connection conn = null;
 		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
+			Class.forName("oracle.jdbc.OracleDriver");
 			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "KH", "KH");
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
